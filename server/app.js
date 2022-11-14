21d76418a205c62517c9b845455a30889483683a
EunGyeong Jung <70186251+EGEG1212@users.noreply.github.com>
2022-11-14 19:34:55 +0900

    update: app.js

--- server/app.js
+++ server/app.js
@@ -1,8 +1,11 @@
-<<<<<<< HEAD
+import dotenv from "dotenv";
+// dotenv.config() 내에 아무것도 명시해주지 않으면 루트에 있는 .env 파일을 찾아 적용한다.
+dotenv.config();
 import express from "express";
 import { Server } from "socket.io";
 import { createServer } from "http";
 import cors from "cors";
+import { sequelize } from "./models/index";
 
 // 서버 4000, 클라이언트 3000
 const PORT = process.env.PORT || 4000;
@@ -10,6 +13,15 @@ const PORT = process.env.PORT || 4000;
 const app = express();
 app.use(express());
 app.use(cors());
+
+sequelize
+  .sync({ force: false }) //기존데이터유지
+  .then(() => {
+    console.log("데이터 베이스 연결 성공");
+  })
+  .catch((err) => {
+    console.error(err);
+  });
 
 // 서버 생성
 const webServer = createServer(app);
@@ -73,28 +85,4 @@ io.on("connection", (socket) => {
     io.emit("newUserResponse", users);
     socket.disconnect();
   });
-=======
-import dotenv from "dotenv";
-// dotenv.config() 내에 아무것도 명시해주지 않으면 루트에 있는 .env 파일을 찾아 적용한다.
-dotenv.config();
-import express from "express";
-import cors from "cors";
-import { sequelize } from "./models/index";
-const app = express();
-const port = process.env.SERVER_PORT || 4000;
-
-app.use(cors());
-
-sequelize
-  .sync({ force: false }) //기존데이터유지
-  .then(() => {
-    console.log("데이터 베이스 연결 성공");
-  })
-  .catch((err) => {
-    console.error(err);
-  });
-
-module.exports = app.listen(port, () => {
-  console.log("Listening ....port", port);
->>>>>>> solo
-});
+})
