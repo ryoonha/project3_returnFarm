956007d7d1b515dfddd441970e45eb4ce4ea8a86
MJKM <rhkrrbaudgg@naver.com>
2022-11-18 00:36:16 +0900

    ✨ add: item folder,  img data

---
+++ client/src/data/item.js
@@ -0,0 +1,14 @@
+export const itemList = {
+  삽: { img: "/images/items/tools/shovel.png" },
+  물뿌리개: { img: "/images/items/tools/wateringTool.png" },
+  해바라기: { img: "/images/items/crops/sunflower.png" },
+  토마토: { img: "/images/items/crops/tomato.png" },
+  옥수수: { img: "/images/items/crops/corn.png" },
+  사과: { img: "/images/items/crops/apple.png" },
+  벼: { img: "/images/items/crops/wheat.png" },
+  옥수수씨앗: { img: "/images/items/crops/cornSeed.png" },
+  토마토씨앗: { img: "/images/items/crops/tomatoSeed.png" },
+  해바라기씨앗: { img: "/images/items/crops/sunflowerSeed.png" },
+  사과씨앗: { img: "/images/items/crops/appleSeed.png" },
+  벼씨앗: { img: "/images/items/crops/wheatSeed.png" },
+};
