fbf59cbed965f751aa065da997fc10eebf1677f9
김륜하 <kimryoonha@ryoon-MacBookPro.local>
2022-11-18 19:49:56 +0900

    ✨add:로그인연장함수,로그인연장에필요한토큰생성함수 새로만듬

--- server/src/router/sign.router.js
+++ server/src/router/sign.router.js
@@ -1,12 +1,18 @@
 import express from "express";
-import { register, login, logout } from "../controller/sign.controller";
+import {
+  register,
+  login,
+  loginExtension,
+  logout,
+} from "../controller/sign.controller";
 
 const router = express.Router();
 
 router.post("/register", register);
-
 router.post("/login", login);
-
-router.get("/logout", logout);
+// router.get("/logout", logout);
+// 재연장해주면서, accessToken 새로 발급해주는 경로
+router.get("/token", loginExtension);
+router.delete("/logout", logout);
 
 export default router;
