8e25016502d17a1bcfeb5599ac13dca9b2410309
MJKM <rhkrrbaudgg@naver.com>
2022-11-22 22:23:52 +0900

    ✨ add:  땅 detailBox 추가

--- client/src/components/canvas/ground/Ground.js
+++ client/src/components/canvas/ground/Ground.js
@@ -1,28 +1,20 @@
 import React from "react";
 import { useSelector } from "react-redux";
 import Tile from "./Tile";
-//import { focusGround } from "../store/reducers/groundSlice";
 
 const Ground = () => {
   const tileArr = useSelector((state) => state.user.tile);
   console.log(tileArr);
-  let arr = Array(10).fill(0);
-  console.log(arr);
 
   return (
     <group position={[0, -1, 0]}>
-      {/* {tileArr.map((data,))} */}
-      {arr.map((e, indexY) => (
-        <group key={"col" + indexY} position={[0, 0, -4.5 + indexY * 5.1]}>
-          {arr.map((e, indexX) => (
-            <Tile
-              key={"row" + indexX}
-              indexX={indexX}
-              indexY={indexY}
-              num={Math.floor(Math.random() * 2)}
-            />
-          ))}
-        </group>
+      {tileArr.map((tileData, index) => (
+        <Tile
+          key={index}
+          tileData={tileData}
+          numX={index % 10}
+          numZ={Math.floor(index / 10)}
+        />
       ))}
     </group>
   );
