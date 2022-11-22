5bd3f7c625a02fca2e8f26ac3daca945662665a2
MJKM <rhkrrbaudgg@naver.com>
2022-11-22 22:26:50 +0900

    ✨ add:  거리에 따른 디테일 박스 닫기 추가

--- client/src/components/character/Man.js
+++ client/src/components/character/Man.js
@@ -1,11 +1,14 @@
 import React, { useEffect, useMemo, useRef, useState } from "react";
 import { useGLTF, useAnimations, Html } from "@react-three/drei";
 import { useFrame, useThree } from "@react-three/fiber";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import Control from "./Control";
 import { Vector3 } from "three";
+import { handleTile } from "../../stores/reducers/stateSlice";
 
 export function Man({ nickName }) {
+  const dispatch = useDispatch();
+  const tilePos = useSelector((state) => state.state.tileSelect);
   const [vec] = useState(() => new Vector3());
   const model = useRef();
   const nameRef = useRef();
@@ -29,6 +32,17 @@ export function Man({ nickName }) {
       // console.log(nick.current.position);
       camera.lookAt(x, y, z + 5);
       camera.position.lerp(vec.set(x, y + 15, z - 15), 0.1);
+
+      if (
+        tilePos.x &&
+        tilePos.z &&
+        (tilePos.x + 10 < x ||
+          tilePos.x - 10 > x ||
+          tilePos.z + 10 < z ||
+          tilePos.z - 10 > z)
+      ) {
+        dispatch(handleTile({ x: null, z: null, data: null }));
+      }
     }
     if (model.current && (up || right || down || left)) {
       if (up && left) {
