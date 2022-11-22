2457c849e696a32209b2233f5d609b04be139175
김륜하 <kimryoonha@ryoon-MacBookPro.local>
2022-11-22 10:17:06 +0900

    🚧 update:pull받기위한commit

--- server/src/controller/sign.controller.js
+++ server/src/controller/sign.controller.js
@@ -5,6 +5,13 @@ import {
   generateRenewToken,
 } from "../middleware/validation";
 import { userRegister, userLogin } from "../db_Process/sign.db";
+import Web3 from "web3";
+
+const web3 = new Web3(
+  new Web3.providers.HttpProvider(
+    "https://goerli.infura.io/v3/b03f802e554f441786b51c437837bfe4%22"
+  )
+);
 
 const register = async (req, res, next) => {
   const { user_id, user_pwd, user_nick } = req.body;
@@ -15,6 +22,9 @@ const register = async (req, res, next) => {
   if (!bool) {
     res.status(409).json({ massage: msg }); // 이미 가입한 유저, conflict
   } else {
+    // 지갑 생성 -> 성공 201, 실패 400
+    const web3Data = web3.eth.accounts.create();
+
     res.status(201).json({ message: "🎉 SUCCESS!" });
   }
 };
@@ -22,27 +32,23 @@ const register = async (req, res, next) => {
 const login = async (req, res, next) => {
   const { user_id, user_pwd } = req.body;
   const logined = await userLogin(user_id, user_pwd);
-  // console.log("🪪", logined.user_nick, logined.address, logined.token_amount); // 출력 확인
-
   if (!logined) {
     return res.status(401).json({ message: "회원가입을먼저해주세요" });
   }
-  // logined에 이미 userLogin 한 값들이 담겨 있음
-  // generateAccessToken -> 첫 로그인 시 주는 access token 생성 함수
-
+  // 신규유저(지갑 없음) -> 지갑 만들어주기
+  // 기존유저(db에서 지갑 있는지 찾아서) -> logined에서 보여줌
   const firstAccessToken = generateAccessToken(
     logined.user_nick,
-    logined.address,
-    logined.token_amount
+    logined.address
   );
-  const refreshToken = generateRefreshToken(logined.user_nick);
-  // console.log(token, " 🔑 처음 발급한 token "); // token이 출력(nick, address, token_amout) 확인
+  // console.log("처음 발급한 access token 🔑 :", firstAccessToken);
+  const refreshToken = generateRefreshToken(logined.user_nick, logined.address);
+  // console.log("처음 발급한 refresh token 🍻 :", refreshToken);
 
   res.status(200).json({
     firstAccessToken,
     refreshToken,
-    nickName: logined.user_nick,
-    message: `Welcome ${logined.user_nick}🥕`,
+    logined,
   });
 };
 
@@ -50,13 +56,16 @@ const login = async (req, res, next) => {
 const loginExtension = async (req, res, next) => {
   // 이미 한 번 이상 로그인 한 회원이므로 로그인 과정 생략
   // 기존 refresh 사용한 access token 발행
-  const { user_id } = req.body;
-  const renewToken = await generateRenewToken(req.headers, user_id);
+  const { refreshToken } = req.body;
+  // refresh token이 발행한 refresh token과 맞다면,
+  // 기존의 refresh token을 서버가 가진 refresh token과 비교해야 할텐데.. 어떻게 비교하지?
+  // 아니면 refresh token 안의 nick, address 와 만료된 access token의 nick, address 비교해서 일치하면 -> 새로운 access token 발급
+  const renewToken = await generateRenewToken(req.body.refreshToken);
+  console.log("👩🏻‍💻", renewToken);
   if (!renewToken) {
     res.sendStatus(412); // 412: 클라이언트의 헤더에 있는 전제조건은 서버의 전제조건에 적절하지 않습니다.
   } else {
-    //console.log(renewToken, "🔓 새로 발급한 token");
-
+    console.log(renewToken, "🔓 두번째 access token");
     res.status(200).json({ message: "재연장 성공!" });
   }
 };
--- server/src/middleware/validation.js
+++ server/src/middleware/validation.js
@@ -2,14 +2,7 @@ import jwt from "jsonwebtoken";
 import dotenv from "dotenv";
 dotenv.config();
 
-// ---------------* 토큰 검정 및 refresh token *---------------
-
-// refresh token 생성
-const generateRefreshToken = (user_nick) => {
-  return jwt.sign({ user_nick }, process.env.REFRESH_SECRET, {
-    expiresIn: "3d",
-  });
-};
+// ----------------------* 토큰 검정  *----------------------
 
 // 로그인 된 상태에서 인증 필요한 페이지 이동 시, 토큰 검증
 const tokenValidation = (req, res, next) => {
@@ -29,13 +22,9 @@ const tokenValidation = (req, res, next) => {
 
 // ---------------------* 로그인 할 때 *---------------------
 
-// generateAccessToken -> 첫 로그인 시 주는 access token 생성 함수
-// nick, address, token_amount
-const generateAccessToken = (user_nick, address, token_amount) => {
-  // 1. refresh token 생성 -> renew에서 비교해보기
-  // 2. access token 생성
+const generateAccessToken = (user_nick, address) => {
   const accessToken = jwt.sign(
-    { user_nick, address, token_amount },
+    { user_nick, address },
     process.env.ACCESS_SECRET,
     {
       expiresIn: "1h",
@@ -45,40 +34,28 @@ const generateAccessToken = (user_nick, address, token_amount) => {
   return accessToken;
 };
 
-// 로그인 연장 토큰(두번째 access, 기존 refresh) 생성
-const generateRenewToken = (headers, id) => {
-  const authorization = headers.authorization; // access 있음
-  const token = authorization.split(" ")[1]; // access 전체
-  // -> token decode해서 refresh secret 맞는지 verify -> access 재발급
+const generateRefreshToken = (user_nick, address) => {
+  return jwt.sign({ user_nick, address }, process.env.REFRESH_SECRET, {
+    expiresIn: "3d",
+  });
+};
 
-  // decode 부분
-  const parseJWT = (token) => {
-    return JSON.parse(Buffer.from(token.split(".")[1], "base64").toString());
-  };
-  const refreshToken = parseJWT(token).refreshToken; // 추출된 refresh token
-
-  // refresh 검증 -> access 발급
-  const result = jwt.verify(refreshToken, process.env.REFRESH_SECRET); // refresh 검증
+// 로그인 연장: req.body의 refresh token이 맞는지 확인해서 새로운 access token 생성 -> 발급
+const generateRenewToken = (req) => {
+  const result = jwt.verify(req.body.refreshToken, process.env.REFRESH_SECRET); // refresh 검증
   if (!result) {
     // 서버에서 발급한 refresh token 아니라면, 에러
     return false;
+  } else {
+    const renewAccessToken = (user_nick, address) => {
+      return jwt.sign({ user_nick, address }, process.env.ACCESS_SECRET, {
+        expiresIn: "1h",
+      });
+    };
+    return renewAccessToken(user_nick, address);
+    // console.log(user_nick, address, "🎉");
   }
-  // refresh 포함, 새로운 access token 생성
-  const finallygenerated = (id, refreshToken) => {
-    return jwt.sign({ id, refreshToken }, process.env.ACCESS_SECRET, {
-      expiresIn: "1h",
-    });
-  };
-  // console.log(id, refreshToken, "🎉");
-  return finallygenerated(id, refreshToken);
 };
-
-/**
-로그인 연장, 두번째 access 발급 과정 로직 
-// 만료 -> access headers 안의 refresh token 암호화 추출
-// refresh token decode -> secret key로 검증 -> 일치하면 
-// 새로운 access token 발급(인자로 req로 받은 id, 기존의 refresh(암호화된상태)를 넣어줌)
- */
 
 export {
   tokenValidation,
