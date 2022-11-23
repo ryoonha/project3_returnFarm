1ff14fb8d31677de9734d1f0e02fd2492fa95ad7
MJKM <rhkrrbaudgg@naver.com>
2022-11-23 23:06:42 +0900

    ✨ add: 상태  정보 표시 박스 생성

---
+++ client/src/components/modals/statusBox/ItemStatus.js
@@ -0,0 +1,38 @@
+import React from "react";
+import { DetailBox } from "../../../libs/cssFrame";
+import { dateName } from "../../../data/weather";
+import { useSelector } from "react-redux";
+
+const ItemStatus = ({ item, index, itemData, dispatch, handleItem }) => {
+  const itemNum = useSelector((state) => state.state.itemSelect);
+
+  if (itemNum === `item${index}`) {
+    return (
+      <DetailBox
+        onMouseEnter={() => {
+          dispatch(handleItem({ itemNum: `item${index}` }));
+        }}
+      >
+        <div className="item cc">{item.item_name}</div>
+        <div className="desc">{itemData[item.item_name].desc}</div>
+        <div className="countBox">
+          <div className="title">개수</div>
+          <div>{item.item_count} 개</div>
+        </div>
+        <div className="timeBox">
+          <div className="title">획득시간</div>
+          <div>
+            {item.time.split("/").map((data, index) => (
+              <span
+                key={index}
+                className={`time${index}`}
+              >{`${data}${dateName[index]} `}</span>
+            ))}
+          </div>
+        </div>
+      </DetailBox>
+    );
+  }
+};
+
+export default ItemStatus;
