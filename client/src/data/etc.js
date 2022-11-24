2f0c14814cf0b5cf1ba37d9ad4de66d564c0e13f
MJKM <rhkrrbaudgg@naver.com>
2022-11-24 17:22:58 +0900

    :contruction: update: 파일 이름 변경

---
+++ client/src/data/etc.js
@@ -0,0 +1,24 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
+export const weatherData = {
+  moon: <FontAwesomeIcon icon="fa-solid fa-moon" />,
+  sun: <FontAwesomeIcon icon="fa-solid fa-sun" />,
+};
+
+export const dateName = {
+  0: "년",
+  1: "월",
+  2: "일",
+  3: "시",
+  4: "분",
+};
+
+export const unitColor = {
+  3: "#A52A2A",
+  4: "#FF7F50",
+  5: "#B8860B",
+  6: "#556B2F",
+  7: "#00CED1",
+  8: "#9400D3",
+  9: "#FF1493",
+};
