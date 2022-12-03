27b5830054d032931e9bd6cf1f3d43ded9ec8d68
MJKM <rhkrrbaudgg@naver.com>
2022-12-03 23:02:48 +0900

    :construction: update: 캐릭터 이름 추가

--- client/src/components/character/Girl.js
+++ client/src/components/character/Girl.js
@@ -1,51 +1,54 @@
-import React, { useEffect, useMemo, useRef, useState } from "react";
-import {
-  useGLTF,
-  useAnimations,
-  Html,
-  PerspectiveCamera,
-} from "@react-three/drei";
-import { useFrame, useThree } from "@react-three/fiber";
+import React, { useEffect, useRef, useState } from "react";
+import { useGLTF, useAnimations, Text } from "@react-three/drei";
+import { useFrame } from "@react-three/fiber";
 import { useDispatch, useSelector } from "react-redux";
 import Control from "./Control";
-import { Vector3 } from "three";
+import { Quaternion, Vector3 } from "three";
 import { handleTile } from "../../stores/reducers/stateSlice";
 import { RigidBody } from "@react-three/rapier";
 
 const moveSpeed = 0.08;
 const runSpeed = 0.2;
-export function Girl() {
+export function Girl({ nickName }) {
   const dispatch = useDispatch();
   const tilePos = useSelector((state) => state.state.tileSelect);
   const [vec] = useState(() => new Vector3());
+  const modelPos = useRef();
   const model = useRef();
-  const group = useRef();
-  const nameRef = useRef();
-  const { camera } = useThree();
+  const text = useRef();
+  const test = useRef();
   const { nodes, materials, animations } = useGLTF(
     "/models/character/girl.gltf"
   );
-
+  // console.log(test);
   // console.log(api);
   const { actions, names } = useAnimations(animations, model);
   const deg2rad = (degrees) => degrees * (Math.PI / 180);
   const { up, right, down, left, shift } = useSelector(
     (state) => state.character
   );
+  const getDirectionOffset = (w, s, d, a) => {
+    let directionOffset = 0; // w
 
-  useFrame(() => {
-    if (model.current) {
-      const { x, y, z } = model.current.position;
-      // console.log(group);
-      // group.current.position.set(x, y, z);
-      // console.log(nameRef);
-      // console.log(nameRef.current.position);
-      // const { nx, ny, nz } = nick.current.position;
-      // nick.current.position.lerp(x, y, z, 0.1);
-      // console.log(nick.current.position);
+    if (w) {
+      // plm
+    } else if (s) {
+      directionOffset = Math.PI; // s
+    } else if (a) {
+      directionOffset = Math.PI / 2; // a
+    } else if (d) {
+      directionOffset = -Math.PI / 2; // d
+    }
+    return directionOffset;
+  };
 
+  useFrame(({ camera }) => {
+    if (modelPos.current) {
+      const { x, y, z } = modelPos.current.position;
+      // console.log(camera);
       camera.lookAt(x, y, z + 5);
       camera.position.lerp(vec.set(x, y + 20, z - 25), 0.1);
+      text.current.quaternion.copy(camera.quaternion);
 
       if (
         tilePos.x &&
@@ -58,81 +61,95 @@ export function Girl() {
         dispatch(handleTile({ x: null, z: null, data: null }));
       }
     }
-    if (model.current && (up || right || down || left)) {
+    if (modelPos.current && (up || right || down || left)) {
       if (up && left) {
         if (shift) {
-          model.current.position.z += runSpeed;
-          model.current.position.x += runSpeed;
+          modelPos.current.position.z += runSpeed;
+          modelPos.current.position.x += runSpeed;
           model.current.rotation.y = deg2rad(35);
         } else {
-          model.current.position.z += moveSpeed;
-          model.current.position.x += moveSpeed;
+          modelPos.current.position.z += moveSpeed;
+          modelPos.current.position.x += moveSpeed;
           model.current.rotation.y = deg2rad(35);
         }
       } else if (up && right) {
         if (shift) {
-          model.current.position.z += runSpeed;
-          model.current.position.x -= runSpeed;
+          modelPos.current.position.z += runSpeed;
+          modelPos.current.position.x -= runSpeed;
           model.current.rotation.y = deg2rad(305);
         } else {
-          model.current.position.z += moveSpeed;
-          model.current.position.x -= moveSpeed;
+          modelPos.current.position.z += moveSpeed;
+          modelPos.current.position.x -= moveSpeed;
           model.current.rotation.y = deg2rad(305);
         }
       } else if (down && left) {
         if (shift) {
-          model.current.position.z -= runSpeed;
-          model.current.position.x += runSpeed;
+          modelPos.current.position.z -= runSpeed;
+          modelPos.current.position.x += runSpeed;
           model.current.rotation.y = deg2rad(125);
         } else {
-          model.current.position.z -= moveSpeed;
-          model.current.position.x += moveSpeed;
+          modelPos.current.position.z -= moveSpeed;
+          modelPos.current.position.x += moveSpeed;
           model.current.rotation.y = deg2rad(125);
         }
       } else if (down && right) {
         if (shift) {
-          model.current.position.z -= runSpeed;
-          model.current.position.x -= runSpeed;
+          modelPos.current.position.z -= runSpeed;
+          modelPos.current.position.x -= runSpeed;
           model.current.rotation.y = deg2rad(215);
         } else {
-          model.current.position.z -= moveSpeed;
-          model.current.position.x -= moveSpeed;
+          modelPos.current.position.z -= moveSpeed;
+          modelPos.current.position.x -= moveSpeed;
           model.current.rotation.y = deg2rad(215);
         }
       } else if (up) {
         if (shift) {
-          model.current.position.z += runSpeed;
+          modelPos.current.position.z += runSpeed;
           model.current.rotation.y = deg2rad(-10);
         } else {
-          model.current.position.z += moveSpeed;
+          modelPos.current.position.z += moveSpeed;
           model.current.rotation.y = deg2rad(-10);
         }
       } else if (right) {
         if (shift) {
-          model.current.position.x -= runSpeed;
+          modelPos.current.position.x -= runSpeed;
           model.current.rotation.y = deg2rad(260);
         } else {
-          model.current.position.x -= moveSpeed;
+          modelPos.current.position.x -= moveSpeed;
           model.current.rotation.y = deg2rad(260);
         }
       } else if (down) {
         if (shift) {
-          model.current.position.z -= runSpeed;
+          modelPos.current.position.z -= runSpeed;
           model.current.rotation.y = deg2rad(170);
         } else {
-          model.current.position.z -= moveSpeed;
+          modelPos.current.position.z -= moveSpeed;
           model.current.rotation.y = deg2rad(170);
         }
       } else if (left) {
         if (shift) {
-          model.current.position.x += runSpeed;
+          modelPos.current.position.x += runSpeed;
           model.current.rotation.y = deg2rad(80);
         } else {
-          model.current.position.x += moveSpeed;
+          modelPos.current.position.x += moveSpeed;
           model.current.rotation.y = deg2rad(80);
         }
       }
     }
+    //model.current.rotation.copy(camera.rotation);
+    // model.current.rotation.set(0, -camera.rotation._y, 0);
+    // let { _x, _y, _z, _w } = camera.quaternion;
+    // let directionOffset = getDirectionOffset(false, true, false, false);
+
+    // const rotateAngleFrontAxis = new Vector3(0, 1, 0);
+    // let rotateQuarternion = new Quaternion();
+    // rotateQuarternion.setFromAxisAngle(rotateAngleFrontAxis, directionOffset);
+    // model.current.quaternion.rotateTowards(rotateQuarternion, 0.2);
+    //  model.current.quaternion.copy(new Quaternion(_x, _y, _z, _w));
+    // console.log(camera.quaternion);
+
+    //console.log(model.current.quaternion);
+    // console.log(camera.quaternion);
   });
   // useEffect(() => {
   //   api.velocity.subscribe((v) => (velocity.current = v));
@@ -156,9 +173,23 @@ export function Girl() {
   }, [up, right, down, left, shift]);
 
   return (
-    <group dispose={null}>
+    // <RigidBody ref={test} type={"fixed"}>
+    <group ref={modelPos} dispose={null} position={[0, 0, -80]}>
       <Control />
-      <group ref={model} name="Scene" position={[0, 0, -80]}>
+      <Text
+        ref={text}
+        color={"rgb(58, 58, 58)"}
+        fontSize={1}
+        maxWidth={100}
+        lineHeight={9}
+        letterSpacing={0.02}
+        textAlign={"left"}
+        anchorX="center"
+        anchorY="bottom"
+      >
+        {nickName}
+      </Text>
+      <group ref={model} name="Scene">
         <group
           name="metarig"
           position={[0, -0.19, -0.03]}
@@ -304,6 +335,7 @@ export function Girl() {
         </group>
       </group>
     </group>
+    // </RigidBody>
   );
 }
 
